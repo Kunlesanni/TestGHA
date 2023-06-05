68668baeb5b3545a80f65cbacdac3cd122deb2b3
osanni <42106750+Kunlesanni@users.noreply.github.com>
2023-06-05 14:54:48 +0100

    #1

--- main.tf
+++ main.tf
@@ -1,49 +1,79 @@
-# terraform {
-#   cloud {
-#     organization = "university-college-london"
+# # terraform {
+# #   cloud {
+# #     organization = "university-college-london"
 
-#     workspaces {
-#       name = "TestGHA"
-#     }
-#   }
-# }
+# #     workspaces {
+# #       name = "TestGHA"
+# #     }
+# #   }
+# # }
+
+# # terraform {
+# #   required_providers {
+# #     azurerm = {
+# #       source  = "hashicorp/azurerm"
+# #       version =   "3.56.0"
+# #     }
+# #   }
+# # }
 
 # terraform {
 #   required_providers {
 #     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version =   "3.56.0"
+#       source = "hashicorp/azurerm"
+#       version = ">= 3.56.0"
 #     }
 #   }
+#   cloud {}
 # }
+
+# # provider "azurerm" {
+# #   features {}
+# #   skip_provider_registration = true
+# # }
+
+
+# provider "azurerm" {
+#         use_cli = true
+#     features {}
+# }
+
+# resource "azurerm_resource_group" "testgha" {
+#       name     = var.rg
+#   location = "uk south"
+#   tags = {
+#     Environment = var.environment
+#   }
+# }
+
+
+
 
 terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">= 3.56.0"
+
     }
   }
   cloud {}
 }
 
-# provider "azurerm" {
-#   features {}
-#   skip_provider_registration = true
-# }
-
 
 provider "azurerm" {
-        use_cli = true
-    features {}
+  features {}
+  # skip_provider_registration = true
 }
 
-resource "azurerm_resource_group" "testgha" {
-      name     = var.rg
-  location = "uk south"
-  tags = {
-    Environment = var.environment
+resource random_password name {
+  length  = 10
+  upper   = true
+  lower   = true
+  number  = true
+  special = true
+
+  keepers = {
+    id = value
   }
 }
-
 
